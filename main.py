f9231b8a8f32b8cdf0aa3040b01126ce088757c5
cutecprog <andi.grooms@gmail.com>
2014-12-09 10:55:46 -0600

    Initalized horizontal_bar function. Define basic version of horizontal_bar

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-        pass
+        for i in range(0,17):
+                print horizontal_bar(i)
+
+def horizontal_bar(length):
+        """Return horizontal_bar the size of length in 1/8 of a column.
+
+        """
+        bar =  '_'*(length/8)
+        if length%8:
+                bar += ','
+        return bar
 
 if __name__=='__main__':
         main() 
