981b360cf0ab0e071b41e8eb021913289ef0374a
cutecprog <andi.grooms@gmail.com>
2014-12-09 12:33:04 -0600

    Write a demo for meter

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
         from time import sleep
-        for i in range(0,256):
-                print meter((1,80), 32, i)
-                sleep(.01)
+        from random import gauss
+        for i in range(0,64):
+                print meter((1,80), 32, sorted([0,int(gauss(128,16)),256])[1])
+                sleep(.083)
 
 def horizontal_bar(length):
         """Return horizontal_bar the size of length in 1/8 of a column
