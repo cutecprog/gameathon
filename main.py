30def4fec2b3e1571d7cb26cb504d0fa090cbc04
cutecprog <andi.grooms@gmail.com>
2014-12-09 17:20:13 -0600

    Move illustrator argument of meter function to illustrate argument of meter function

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
                 print loc(1,80) + meter(hbar, 32, gaussed(256, 4, (i/3)*12))
                 sleep(.083)
         wind_speed = GraphicVar((20, 80), 512, 4)
+        print wind_speed.get()
+        wind_speed.set(9)
+        print wind_speed.get()
 
 if __name__=='__main__':
         main() 
