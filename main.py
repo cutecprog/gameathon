f1a897918fbbbd0eacef23a5d014a946a98a8917
cutecprog <andi.grooms@gmail.com>
2014-12-10 11:35:37 -0600

    Convert demo in main.py file to use display loop

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from lib.display import hbar, GraphicVar, display_loop
 def main():
         from time import sleep
         wind_speed = GraphicVar((1, 80), hbar,  32, 4)
+        display_loop(wind_speed)
         for i in range(0,64):
                 wind_speed.value = (i/3)*12
-                print wind_speed
                 sleep(.083)
-        display_loop(wind_speed)
-        sleep(1)
 
 if __name__=='__main__':
         main() 
