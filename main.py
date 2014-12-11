19c0407b7fb1954f1ee0930e76952867f5bc478d
cutecprog <andi.grooms@gmail.com>
2014-12-11 16:17:38 -0600

    Improve demo in main.py file

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from lib.display import hbar, GraphicVar
 
 def main():
         from time import sleep
-        wind_speed = GraphicVar((1, 80), hbar,  32, 4)
-        wind_speed.show()
+        pressure_valve = GraphicVar((1, 80), hbar,  32, 4)
+        pressure_valve2 = GraphicVar((2, 80), hbar,  32, 0)
+        pressure_valve.show()
+        pressure_valve2.show()
         for i in range(0,256):
-                wind_speed.value = i
+                pressure_valve.value = i
+                pressure_valve2.value = i
                 sleep(.04)
-        wind_speed.hide()
+        sleep(1)
+        pressure_valve.hide()
+        pressure_valve2.hide()
 
 if __name__=='__main__':
         main() 
