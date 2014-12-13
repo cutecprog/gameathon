ebfede2778506163b8dec1ed4f1f146ce8262622
cutecprog <andi.grooms@gmail.com>
2014-12-13 07:46:19 -0600

    Remove GraghicVar demo from main.py file. Make getch demo in main.py file

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 """Hold game code in this file"""
 
-from lib.display import hbar, GraphicVar, meter, loc
+from lib.display import loc
 from lib.keyboard import getch
 
 def main():
-        from time import sleep
-        pressure_valve  = GraphicVar((1, 80), hbar,  32, 4)
-        pressure_valve2 = GraphicVar((2, 80), hbar,  32, 0)
-        pressure_valve.show()
-        pressure_valve2.show()
-        for i in range(0,264):
-                pressure_valve.value  = i
-                pressure_valve2.value = i
-                print loc(3,80) + str((i/8)*8) + ' + ' + meter(hbar,1,i%8,)
-                sleep(.04)
-        print getch()
-        print loc(3,80) + "       "
-        pressure_valve.hide()
-        pressure_valve2.hide()
+        ch = ""
+        while ch != '\033':
+                ch = getch()
+                msg = loc(5,80)
+                print msg + ' '*8
+                for c in ch:
+                        msg += c + ' '
+                print msg
 
 if __name__=='__main__':
         main() 
