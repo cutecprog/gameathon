9bb8dacc45ae163384a2e594a72cf3fe53557af4
cutecprog <andi.grooms@gmail.com>
2014-12-13 10:05:46 -0600

    In big boy step fashion, modify demo to recurse until player wins and modify demo to clear screen at the start of a recurse and clean up the code a little

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from lib.display import loc, hbar, GraphicVar
 from lib.keyboard import getch
 
 def main():
+        from random import randint
+        from os import system
+        system('clear')
         ch = ""
         pedal = GraphicVar((2,80), hbar, 32, 0)
         gauge = GraphicVar((1,80), hbar, 32, 4)
         pedal.value = 128
-        gauge.value = 37
+        gauge.value = randint(0,255)
         pedal.show()
         gauge.show()
         start_value = pedal.value
         n = start_value
-        msg = loc(5,80)
-        while ch != '\033':
-                print msg + str(int(pedal.value))
+        while ch != '\r':
                 ch = getch()
                 if ch[:2] == '\033[':
                         if ch[2] == 'D':   # Left Arrow
@@ -27,13 +28,15 @@ def main():
                         elif ch[2] == 'A': # Up Arrow
                                 pedal.value = start_value
                                 n = start_value
-                print msg + ' '*3
         pedal.hide()
         gauge.hide()
         if pedal.value == gauge.value:
                 print 'You win'
+                return
         else:
-                print 'You lose'
+                print 'You loss by', gauge.value - pedal.value
+        getch()
+        main()
 
 if __name__=='__main__':
         main() 
