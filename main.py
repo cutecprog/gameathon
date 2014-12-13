6c55bd96770fe3ec4a907c6b64803962ea4e7c12
cutecprog <andi.grooms@gmail.com>
2014-12-13 09:33:18 -0600

    Modify demo to where it actually does a binary search for the value

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ def main():
         pedal.value = 128
         pedal.show()
         start_value = pedal.value
-        num_left  = 0
-        num_right = 0
+        n = 1
         while ch != '\033':
                 ch = getch()
                 if ch[:2] == '\033[':
                         if ch[2] == 'D':   # Left Arrow
-                                num_left += 1
+                                n *= 2
+                                pedal.value -= start_value/n
                         elif ch[2] == 'C': # Right Arrow
-                                num_right += 1
+                                n *= 2
+                                pedal.value += start_value/n
                         elif ch[2] == 'A': # Up Arrow
                                 pedal.value = start_value
-                                num_left = 0
-                                num_right = 0
+                                n = 1
                 msg = loc(5,80)
                 print msg + ' '*3
                 print msg + str(int(pedal.value))
