9e02725e83c7f945583801f96f2d024f5aa23650
cutecprog <andi.grooms@gmail.com>
2014-12-13 07:56:46 -0600

    Add pedal GraphicVar object to demo.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """Hold game code in this file"""
 
-from lib.display import loc
+from lib.display import loc, hbar, GraphicVar
 from lib.keyboard import getch
 
 def main():
         ch = ""
+        pedal = GraphicVar((2,80), hbar, 32, 0)
+        pedal.value = 128
+        pedal.show()
         while ch != '\033':
                 ch = getch()
                 msg = loc(5,80)
@@ -12,6 +15,7 @@ def main():
                 for c in ch:
                         msg += c + ' '
                 print msg
+        pedal.hide()
 
 if __name__=='__main__':
         main() 
