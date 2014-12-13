98dd72e0331be2577e6d0780701cbaeae2343d62
cutecprog <andi.grooms@gmail.com>
2014-12-13 09:55:50 -0600

    Initialize and define gauge GraphicVar object in demo. Add win/lose state in demo

--- main.py
+++ main.py
@@ -6,27 +6,34 @@ from lib.keyboard import getch
 def main():
         ch = ""
         pedal = GraphicVar((2,80), hbar, 32, 0)
+        gauge = GraphicVar((1,80), hbar, 32, 4)
         pedal.value = 128
+        gauge.value = 37
         pedal.show()
+        gauge.show()
         start_value = pedal.value
-        n = 1
+        n = start_value
+        msg = loc(5,80)
         while ch != '\033':
+                print msg + str(int(pedal.value))
                 ch = getch()
                 if ch[:2] == '\033[':
                         if ch[2] == 'D':   # Left Arrow
-                                n *= 2
-                                pedal.value -= start_value/n
+                                n /= 2
+                                pedal.value -= n
                         elif ch[2] == 'C': # Right Arrow
-                                n *= 2
-                                pedal.value += start_value/n
+                                n /= 2
+                                pedal.value += n
                         elif ch[2] == 'A': # Up Arrow
                                 pedal.value = start_value
-                                n = 1
-                msg = loc(5,80)
+                                n = start_value
                 print msg + ' '*3
-                print msg + str(int(pedal.value))
-                
         pedal.hide()
+        gauge.hide()
+        if pedal.value == gauge.value:
+                print 'You win'
+        else:
+                print 'You lose'
 
 if __name__=='__main__':
         main() 
