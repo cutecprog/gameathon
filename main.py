f7c18f110857f2c512ac4b7cedfb43635238570d
cutecprog <andi.grooms@gmail.com>
2014-12-14 10:31:35 -0600

    Modify demo to be iterative instead of recursive and change exit condition to be based on how far off player is

--- main.py
+++ main.py
@@ -7,26 +7,21 @@ def main():
         from random import randint
         from os import system
         system('clear')
-        pedal = GraphicVar((2,80), hbar, 32)
-        gauge = GraphicVar((1,80), hbar, 32, 4)
-        gauge.value = randint(0,255)
-        pedal.show()
-        gauge.show()
-        pedal.bs_input()
-        gauge.sigma = 0
-        if pedal.value == gauge.value:
-                print 'You win'
+        score = 0
+        loss  = 0
+        while loss < 144:
+                print loc(1, 100) + "Score:", score
+                pedal = GraphicVar((3,80), hbar, 32)
+                gauge = GraphicVar((2,80), hbar, 32, 4)
+                gauge.value = randint(0,255)
+                pedal.show()
+                gauge.show()
+                pedal.bs_input()
+                gauge.sigma = 0
+                loss += (gauge.value - pedal.value)**2
                 getch()
                 pedal.hide()
                 gauge.hide()
-                system('clear')
-                return
-        else:
-                print 'You loss by', pedal.value - gauge.value
-        getch()
-        pedal.hide()
-        gauge.hide()
-        main()
 
 if __name__=='__main__':
         main() 
