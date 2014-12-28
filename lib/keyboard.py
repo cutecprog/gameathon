2457cd6f55401739915c5a79afa171558ba09729
cutecprog <andi.grooms@gmail.com>
2014-12-28 17:30:01 -0600

    Print attr trying to assign in __setattr__ method in Cursor class

--- lib/keyboard.py
+++ lib/keyboard.py
@@ -50,6 +50,5 @@ class Cursor(object):
                 return loc(self.position) + self.symbol
 
         def __setattr__(self, name, value):
-                #if name == 'position':
-                #        print loc(self.position) + ' '
+                print repr(name)
                 self.__dict__[name] = value
