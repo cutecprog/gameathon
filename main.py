4dd8f06ac610a8ed23c4f1e4c7e54a6f8c8d8461
cutecprog <andi.grooms@gmail.com>
2014-12-28 17:13:59 -0600

    Call getch function in infinite loop. Bind ESC to print ch variable. Bind q to break from the infinite loop

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 """Hold game code in this file"""
 
-from lib.keyboard import getch, Cursor
+from lib.keyboard import getch, Cursor, Key
 
 def main():
         print getch()
-        a = Cursor([80,80], '<')
+        a = Cursor([20,80], '<')
         print a.position
         print a.symbol
         print a
+        while True:
+                ch = getch()
+                if ch[0] == Key.ESC:
+                        print ch[1:]
+                elif ch == 'q':
+                        break
 
 if __name__=='__main__':
         main()
