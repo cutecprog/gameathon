ef7e739b65b9de8dbce791c793dc623bcf99cebe
cutecprog <andi.grooms@gmail.com>
2014-12-28 17:18:08 -0600

    Bind arrow keys to logically manipulate position property of a Cursor object

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ def main():
         print a
         while True:
                 ch = getch()
+                if ch == Key.LEFT_ARROW:
+                        a.position[1] -= 1
+                elif ch == Key.RIGHT_ARROW:
+                        a.position[1] += 1
+                elif ch == Key.UP_ARROW:
+                        a.position[0] -= 1
+                elif ch == Key.DOWN_ARROW:
+                        a.position[0] += 1
                 if ch[0] == Key.ESC:
                         print ch[1:]
                 elif ch == 'q':
                         break
+                print a
 
 if __name__=='__main__':
         main()
