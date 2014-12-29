b01d8ddd628718fe4a41ed831d43dce45f7a8e67
cutecprog <andi.grooms@gmail.com>
2014-12-29 13:53:56 -0600

    Initialize and define Player class

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 """Hold game code in this file"""
 
 def main():
-        print "Start"
+        print me()
+
+class me(object):
+        def __init__(self, pos = [0,0]):
+                self.pos = pos
+        def __repr__(self):
+                y, x = self.pos
+                if y%2 == 0:
+                        return '\033[%s;%sH\xe2\x81\xb0' % (str(y/2), str(x))
+                else:
+                        return '\033[%s;%sH\xe2\x82\x80' % (str(y/2), str(x))
 
 if __name__=='__main__':
         main()
