2370b793b863ad48b8687c277714083c950c1041
cutecprog <andi.grooms@gmail.com>
2014-12-29 14:09:18 -0600

    Add keypad control loop to demo

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 """Hold game code in this file"""
 
 def main():
+        from lib.keyboard import getch, Key
+        from lib.display import loc
         me = Player([80,80])
         print me
+        while True:
+                ch = getch()
+                if ch == Key.UP_ARROW:
+                        #y,x = me.pos
+                        #if y%2:
+                        #        print loc(y,x) + ' '
+                        me.pos[0] -= 1
+                elif ch == Key.DOWN_ARROW:
+                        #y,x = me.pos
+                        #if y%2:
+                        #        print loc(y,x) + ' '
+                        me.pos[0] += 1
+                elif ch == Key.LEFT_ARROW:
+                        #y,x = me.pos
+                        #print loc(y,x) + ' '
+                        me.pos[1] -= 1
+                elif ch == Key.RIGHT_ARROW:
+                        #y,x = me.pos
+                        #print loc(y,x) + ' '
+                        me.pos[1] += 1
+                elif ch == 'q':
+                        break
+                print me
 
 class Player(object):
         def __init__(self, pos = [0,0]):
