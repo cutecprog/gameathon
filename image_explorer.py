a7d770e0359da9d8baf9ece343face1e0e440527
cutecprog <andi.grooms@gmail.com>
2015-01-22 12:12:33 -0600

    Initialize and define display_loop function. Initialize and define display_thread Thread object with target=display_loop

--- image_explorer.py
+++ image_explorer.py
@@ -3,6 +3,7 @@ from random import random
 from lib.keyboard import getch
 from lib.display import loc, GraphicVar
 from os import system
+from threading import Thread
 
 def dither(x):
         n = int(x)
@@ -26,7 +27,9 @@ def saturate(n, h, l=0):
         else:
                 return n
 
-def print_pixels(pix, x,y, view):
+def print_pixels(pix, pos, view):
+        print loc(1, 80), pos, ' '*8
+        x, y = pos
         for r in range(-view, view+1):
                 for c in range(-(2*view), (2*view)+1):
                         if c==0 and r ==0:
@@ -34,21 +37,31 @@ def print_pixels(pix, x,y, view):
                         print loc(16+r,30+c) + foo(pix[x+c,y+r])
                         print '\033[0m'        
 
+def display_loop(pix, pos, view):
+        while pos != [0,0]:
+                print_pixels(pix, pos, view)
+
 def explore(im, view):
         ch = ''
         pix = im.load()
         x_max, y_max = im.size
-        x, y = (20,20)
+        pos = [20,20]
         key_binds = {'\x1b[D': (0, -1), '\x1b[A': (-1, 0),                    \
                      '\x1b[C': (0, 1), '\x1b[B': (1,0)}
+        display_thread = Thread(target=display_loop, args=[pix,pos,view])
+        display_thread.start()
         while ch != 'q':
                 ch=getch()
                 if ch in key_binds:
+                        x, y = pos
                         y_offset, x_offset = key_binds[ch]
                         x = saturate(x+x_offset, x_max-view, view)
                         y = saturate(y+y_offset, y_max-view, view)
-                print loc(1, 80), x,y, ' '*8
-                print_pixels(pix, x,y, view)
+                        pos[0] = x
+                        pos[1] = y
+                #print_pixels(pix, pos, view)
+        pos[0] = 0
+        pos[1] = 0
 
 im = Image.open('../tl1.png')
 system('clear')
